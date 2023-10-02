b202d5a38f95862597bfa5635e789e6a02563bbe
David Trompa <dacardo4@gmail.com>
2023-10-01 23:31:15 -0500

    inicio completo

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  ngOnInit() {
+    // Este código carga el reproductor de la API en un iframe de manera asíncrona, siguiendo las instrucciones:
+    // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+    const tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(tag);
+  }
 
 }
